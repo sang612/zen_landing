9fe97b498152d22e0a7dbd27b57defbc552ed9a1
liamtrn096 <liamtrn096@gmail.com>
2023-05-29 16:25:48 +0700

    Improvement - Change mail to config

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,6 +1,7 @@
 import nodemailer from "nodemailer";
 
 const email = process.env.EMAIL;
+const emailTo = process.env.EMAIL_TO;
 const pass = process.env.EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
@@ -13,5 +14,5 @@ export const transporter = nodemailer.createTransport({
 
 export const mailOptions = {
 	from: email,
-	to: email,
+	to: emailTo,
 };

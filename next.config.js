0aa5bd2ca96b10c976c92e3f138acd05bcf4d091
liamtrn096 <liamtrn096@gmail.com>
2023-06-28 14:34:59 +0700

    Improvement - Case study: Add env config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		appDir: true,
-	},
-	env: {
-		NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL:
-			process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
-		NEXT_PUBLIC_GOOGLE_PRIVATE_KEY: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY,
-		NEXT_PUBLIC_GOOGLE_SHEET_ID: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID,
-	},
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL:
+      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_EMAIL,
+    NEXT_PUBLIC_GOOGLE_PRIVATE_KEY: process.env.NEXT_PUBLIC_GOOGLE_PRIVATE_KEY,
+    NEXT_PUBLIC_GOOGLE_SHEET_ID: process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
